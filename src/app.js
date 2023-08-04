685c4fa8fc4e0a29447a19bcba8daa092dd19270
Geraldo Gomes <contato.gneto@gmail.com>
2023-08-04 19:47:57 -0300

    add local repository in cloud

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+const PORT = process.env.port || 5000
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
