c5223e1b19e1f02666e505ec81272e79a7e0849a
Geraldo Gomes <contato.gneto@gmail.com>
2023-08-07 16:57:45 -0300

    chore: adjust code for deploy on render

--- dump.sql
+++ dump.sql
@@ -2,8 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.11 (Ubuntu 13.11-1.pgdg20.04+1)
+-- Dumped from database version 15.3 (Ubuntu 15.3-0ubuntu0.23.04.1)
 -- Dumped by pg_dump version 15.3 (Ubuntu 15.3-0ubuntu0.23.04.1)
+
+-- Started on 2023-08-07 16:56:29 -03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,315 +18,13 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
---
-
--- *not* creating schema, since initdb creates it
-
-
-ALTER SCHEMA public OWNER TO postgres;
-
---
--- Name: btree_gin; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS btree_gin WITH SCHEMA public;
-
-
---
--- Name: EXTENSION btree_gin; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION btree_gin IS 'support for indexing common datatypes in GIN';
-
-
---
--- Name: btree_gist; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS btree_gist WITH SCHEMA public;
-
-
---
--- Name: EXTENSION btree_gist; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION btree_gist IS 'support for indexing common datatypes in GiST';
-
-
---
--- Name: citext; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
-
-
---
--- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings';
-
-
---
--- Name: cube; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS cube WITH SCHEMA public;
-
-
---
--- Name: EXTENSION cube; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION cube IS 'data type for multidimensional cubes';
-
-
---
--- Name: dblink; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS dblink WITH SCHEMA public;
-
-
---
--- Name: EXTENSION dblink; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION dblink IS 'connect to other PostgreSQL databases from within a database';
-
-
---
--- Name: dict_int; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS dict_int WITH SCHEMA public;
-
-
---
--- Name: EXTENSION dict_int; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION dict_int IS 'text search dictionary template for integers';
-
-
---
--- Name: dict_xsyn; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS dict_xsyn WITH SCHEMA public;
-
-
---
--- Name: EXTENSION dict_xsyn; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION dict_xsyn IS 'text search dictionary template for extended synonym processing';
-
-
---
--- Name: earthdistance; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS earthdistance WITH SCHEMA public;
-
-
---
--- Name: EXTENSION earthdistance; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION earthdistance IS 'calculate great-circle distances on the surface of the Earth';
-
-
---
--- Name: fuzzystrmatch; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS fuzzystrmatch WITH SCHEMA public;
-
-
---
--- Name: EXTENSION fuzzystrmatch; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION fuzzystrmatch IS 'determine similarities and distance between strings';
-
-
---
--- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
-
-
---
--- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
-
-
---
--- Name: intarray; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS intarray WITH SCHEMA public;
-
-
---
--- Name: EXTENSION intarray; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION intarray IS 'functions, operators, and index support for 1-D arrays of integers';
-
-
---
--- Name: ltree; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS ltree WITH SCHEMA public;
-
-
---
--- Name: EXTENSION ltree; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION ltree IS 'data type for hierarchical tree-like structures';
-
-
---
--- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
-
-
---
--- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pg_trgm; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION pg_trgm IS 'text similarity measurement and index searching based on trigrams';
-
-
---
--- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
-
-
---
--- Name: pgrowlocks; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS pgrowlocks WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pgrowlocks; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION pgrowlocks IS 'show row-level locking information';
-
-
---
--- Name: pgstattuple; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pgstattuple; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION pgstattuple IS 'show tuple-level statistics';
-
-
---
--- Name: tablefunc; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS tablefunc WITH SCHEMA public;
-
-
---
--- Name: EXTENSION tablefunc; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION tablefunc IS 'functions that manipulate whole tables, including crosstab';
-
-
---
--- Name: unaccent; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
-
-
---
--- Name: EXTENSION unaccent; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
-
-
---
--- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
-
-
---
--- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
-
-
---
--- Name: xml2; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS xml2 WITH SCHEMA public;
-
-
---
--- Name: EXTENSION xml2; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION xml2 IS 'XPath querying and XSLT';
-
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: urls; Type: TABLE; Schema: public; Owner: vghvwvps
+-- TOC entry 217 (class 1259 OID 16945)
+-- Name: urls; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.urls (
@@ -337,10 +37,11 @@ CREATE TABLE public.urls (
 );
 
 
-ALTER TABLE public.urls OWNER TO vghvwvps;
+ALTER TABLE public.urls OWNER TO postgres;
 
 --
--- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: vghvwvps
+-- TOC entry 216 (class 1259 OID 16944)
+-- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE public.urls_id_seq
@@ -352,17 +53,20 @@ CREATE SEQUENCE public.urls_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.urls_id_seq OWNER TO vghvwvps;
+ALTER TABLE public.urls_id_seq OWNER TO postgres;
 
 --
--- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vghvwvps
+-- TOC entry 3368 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: vghvwvps
+-- TOC entry 215 (class 1259 OID 16936)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.users (
@@ -375,10 +79,11 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO vghvwvps;
+ALTER TABLE public.users OWNER TO postgres;
 
 --
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: vghvwvps
+-- TOC entry 214 (class 1259 OID 16935)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE public.users_id_seq
@@ -390,63 +95,80 @@ CREATE SEQUENCE public.users_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_id_seq OWNER TO vghvwvps;
+ALTER TABLE public.users_id_seq OWNER TO postgres;
 
 --
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vghvwvps
+-- TOC entry 3369 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: urls id; Type: DEFAULT; Schema: public; Owner: vghvwvps
+-- TOC entry 3209 (class 2604 OID 16948)
+-- Name: urls id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id_seq'::regclass);
 
 
 --
--- Name: users id; Type: DEFAULT; Schema: public; Owner: vghvwvps
+-- TOC entry 3206 (class 2604 OID 16939)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
 --
--- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: vghvwvps
+-- TOC entry 3362 (class 0 OID 16945)
+-- Dependencies: 217
+-- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.urls (id, "idUser", "shortUrl", url, "visitCount", "createdAt") FROM stdin;
-1	1	E7ORGoFm5x	https://google.com.br	2	2023-08-07 19:24:08.137405
+2	1	zopRG2Iz_p	https://google.com.br	2	2023-08-07 05:56:47.30614
+3	2	blG-uxCIsY	https://google.com.br	1	2023-08-07 05:58:07.682735
+4	2	-mXa3ALEA6	https://gdoogle.com.br	0	2023-08-07 07:33:40.186905
+5	1	fAqnTYekht	https://gdsdfsdfoogle.com.br	3	2023-08-07 08:06:19.662442
 \.
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: vghvwvps
+-- TOC entry 3360 (class 0 OID 16936)
+-- Dependencies: 215
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.users (id, name, email, password, "linksCount", "createdAt") FROM stdin;
-1	Carlos	contato.gneto@gmail.com	$2b$10$nDP3M9x7qu8LlkxnLIz3RuMO.Rz2rCTrtuyRP7gaeOlU13P7jS3ni	1	2023-08-07 19:15:10.27291
+2	Carlos	contato.gneto@gmail.com	$2b$10$eCaZFTl40d7W5jZKgGZk2u7Hm8OyJcu1jNdcQnp9uQSaw.wUuD9Qm	2	2023-08-07 05:57:48.44828
+1	Carlos	contato.carloshg@gmail.com	$2b$10$tuEgaGg61jMYhUwZp5MqwexYS/e3NCUo/.tOrky4fwqiIT66HwQwO	2	2023-08-07 04:11:27.505521
 \.
 
 
 --
--- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vghvwvps
+-- TOC entry 3370 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 1, true);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vghvwvps
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 5, true);
 
 
 --
--- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: vghvwvps
+-- TOC entry 3371 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+
+
+--
+-- TOC entry 3215 (class 2606 OID 16954)
+-- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.urls
@@ -454,7 +176,8 @@ ALTER TABLE ONLY public.urls
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: vghvwvps
+-- TOC entry 3213 (class 2606 OID 16943)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users
@@ -462,20 +185,15 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: urls urls_idUser_fkey; Type: FK CONSTRAINT; Schema: public; Owner: vghvwvps
+-- TOC entry 3216 (class 2606 OID 16955)
+-- Name: urls urls_idUser_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.urls
     ADD CONSTRAINT "urls_idUser_fkey" FOREIGN KEY ("idUser") REFERENCES public.users(id);
 
 
---
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE USAGE ON SCHEMA public FROM PUBLIC;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
+-- Completed on 2023-08-07 16:56:29 -03
 
 --
 -- PostgreSQL database dump complete
