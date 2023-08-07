79a687972eaf760614d5a6210404551499b42804
Geraldo Gomes <contato.gneto@gmail.com>
2023-08-07 16:52:50 -0300

    chore: adjust code for deploy on render

--- dump.sql
+++ dump.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 13.11 (Ubuntu 13.11-1.pgdg20.04+1)
 -- Dumped by pg_dump version 15.3 (Ubuntu 15.3-0ubuntu0.23.04.1)
 
--- Started on 2023-08-07 16:40:07 -03
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -19,7 +17,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 25 (class 2615 OID 2200)
 -- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
 --
 
@@ -29,7 +26,6 @@ SET row_security = off;
 ALTER SCHEMA public OWNER TO postgres;
 
 --
--- TOC entry 15 (class 3079 OID 17161)
 -- Name: btree_gin; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -37,8 +33,6 @@ CREATE EXTENSION IF NOT EXISTS btree_gin WITH SCHEMA public;
 
 
 --
--- TOC entry 4108 (class 0 OID 0)
--- Dependencies: 15
 -- Name: EXTENSION btree_gin; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -46,7 +40,6 @@ COMMENT ON EXTENSION btree_gin IS 'support for indexing common datatypes in GIN'
 
 
 --
--- TOC entry 19 (class 3079 OID 17702)
 -- Name: btree_gist; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -54,8 +47,6 @@ CREATE EXTENSION IF NOT EXISTS btree_gist WITH SCHEMA public;
 
 
 --
--- TOC entry 4109 (class 0 OID 0)
--- Dependencies: 19
 -- Name: EXTENSION btree_gist; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -63,7 +54,6 @@ COMMENT ON EXTENSION btree_gist IS 'support for indexing common datatypes in GiS
 
 
 --
--- TOC entry 8 (class 3079 OID 16671)
 -- Name: citext; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -71,8 +61,6 @@ CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
 
 
 --
--- TOC entry 4110 (class 0 OID 0)
--- Dependencies: 8
 -- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -80,7 +68,6 @@ COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings
 
 
 --
--- TOC entry 17 (class 3079 OID 17599)
 -- Name: cube; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -88,8 +75,6 @@ CREATE EXTENSION IF NOT EXISTS cube WITH SCHEMA public;
 
 
 --
--- TOC entry 4111 (class 0 OID 0)
--- Dependencies: 17
 -- Name: EXTENSION cube; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -97,7 +82,6 @@ COMMENT ON EXTENSION cube IS 'data type for multidimensional cubes';
 
 
 --
--- TOC entry 2 (class 3079 OID 16384)
 -- Name: dblink; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -105,8 +89,6 @@ CREATE EXTENSION IF NOT EXISTS dblink WITH SCHEMA public;
 
 
 --
--- TOC entry 4112 (class 0 OID 0)
--- Dependencies: 2
 -- Name: EXTENSION dblink; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -114,7 +96,6 @@ COMMENT ON EXTENSION dblink IS 'connect to other PostgreSQL databases from withi
 
 
 --
--- TOC entry 14 (class 3079 OID 17152)
 -- Name: dict_int; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -122,8 +103,6 @@ CREATE EXTENSION IF NOT EXISTS dict_int WITH SCHEMA public;
 
 
 --
--- TOC entry 4113 (class 0 OID 0)
--- Dependencies: 14
 -- Name: EXTENSION dict_int; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -131,7 +110,6 @@ COMMENT ON EXTENSION dict_int IS 'text search dictionary template for integers';
 
 
 --
--- TOC entry 20 (class 3079 OID 18325)
 -- Name: dict_xsyn; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -139,8 +117,6 @@ CREATE EXTENSION IF NOT EXISTS dict_xsyn WITH SCHEMA public;
 
 
 --
--- TOC entry 4114 (class 0 OID 0)
--- Dependencies: 20
 -- Name: EXTENSION dict_xsyn; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -148,7 +124,6 @@ COMMENT ON EXTENSION dict_xsyn IS 'text search dictionary template for extended 
 
 
 --
--- TOC entry 18 (class 3079 OID 17686)
 -- Name: earthdistance; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -156,8 +131,6 @@ CREATE EXTENSION IF NOT EXISTS earthdistance WITH SCHEMA public;
 
 
 --
--- TOC entry 4115 (class 0 OID 0)
--- Dependencies: 18
 -- Name: EXTENSION earthdistance; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -165,7 +138,6 @@ COMMENT ON EXTENSION earthdistance IS 'calculate great-circle distances on the s
 
 
 --
--- TOC entry 7 (class 3079 OID 16660)
 -- Name: fuzzystrmatch; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -173,8 +145,6 @@ CREATE EXTENSION IF NOT EXISTS fuzzystrmatch WITH SCHEMA public;
 
 
 --
--- TOC entry 4116 (class 0 OID 0)
--- Dependencies: 7
 -- Name: EXTENSION fuzzystrmatch; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -182,7 +152,6 @@ COMMENT ON EXTENSION fuzzystrmatch IS 'determine similarities and distance betwe
 
 
 --
--- TOC entry 13 (class 3079 OID 17025)
 -- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -190,8 +159,6 @@ CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
 
 
 --
--- TOC entry 4117 (class 0 OID 0)
--- Dependencies: 13
 -- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -199,7 +166,6 @@ COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs
 
 
 --
--- TOC entry 12 (class 3079 OID 16903)
 -- Name: intarray; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -207,8 +173,6 @@ CREATE EXTENSION IF NOT EXISTS intarray WITH SCHEMA public;
 
 
 --
--- TOC entry 4118 (class 0 OID 0)
--- Dependencies: 12
 -- Name: EXTENSION intarray; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -216,7 +180,6 @@ COMMENT ON EXTENSION intarray IS 'functions, operators, and index support for 1-
 
 
 --
--- TOC entry 4 (class 3079 OID 16444)
 -- Name: ltree; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -224,8 +187,6 @@ CREATE EXTENSION IF NOT EXISTS ltree WITH SCHEMA public;
 
 
 --
--- TOC entry 4119 (class 0 OID 0)
--- Dependencies: 4
 -- Name: EXTENSION ltree; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -233,7 +194,6 @@ COMMENT ON EXTENSION ltree IS 'data type for hierarchical tree-like structures';
 
 
 --
--- TOC entry 22 (class 3079 OID 18337)
 -- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -241,8 +201,6 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 
 
 --
--- TOC entry 4120 (class 0 OID 0)
--- Dependencies: 22
 -- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -250,7 +208,6 @@ COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statist
 
 
 --
--- TOC entry 11 (class 3079 OID 16824)
 -- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -258,8 +215,6 @@ CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
 
 
 --
--- TOC entry 4121 (class 0 OID 0)
--- Dependencies: 11
 -- Name: EXTENSION pg_trgm; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -267,7 +222,6 @@ COMMENT ON EXTENSION pg_trgm IS 'text similarity measurement and index searching
 
 
 --
--- TOC entry 10 (class 3079 OID 16787)
 -- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -275,8 +229,6 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
 
 
 --
--- TOC entry 4122 (class 0 OID 0)
--- Dependencies: 10
 -- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -284,7 +236,6 @@ COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 
 
 --
--- TOC entry 16 (class 3079 OID 17597)
 -- Name: pgrowlocks; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -292,8 +243,6 @@ CREATE EXTENSION IF NOT EXISTS pgrowlocks WITH SCHEMA public;
 
 
 --
--- TOC entry 4123 (class 0 OID 0)
--- Dependencies: 16
 -- Name: EXTENSION pgrowlocks; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -301,7 +250,6 @@ COMMENT ON EXTENSION pgrowlocks IS 'show row-level locking information';
 
 
 --
--- TOC entry 5 (class 3079 OID 16629)
 -- Name: pgstattuple; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -309,8 +257,6 @@ CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA public;
 
 
 --
--- TOC entry 4124 (class 0 OID 0)
--- Dependencies: 5
 -- Name: EXTENSION pgstattuple; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -318,7 +264,6 @@ COMMENT ON EXTENSION pgstattuple IS 'show tuple-level statistics';
 
 
 --
--- TOC entry 6 (class 3079 OID 16639)
 -- Name: tablefunc; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -326,8 +271,6 @@ CREATE EXTENSION IF NOT EXISTS tablefunc WITH SCHEMA public;
 
 
 --
--- TOC entry 4125 (class 0 OID 0)
--- Dependencies: 6
 -- Name: EXTENSION tablefunc; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -335,7 +278,6 @@ COMMENT ON EXTENSION tablefunc IS 'functions that manipulate whole tables, inclu
 
 
 --
--- TOC entry 21 (class 3079 OID 18330)
 -- Name: unaccent; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -343,8 +285,6 @@ CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
 
 
 --
--- TOC entry 4126 (class 0 OID 0)
--- Dependencies: 21
 -- Name: EXTENSION unaccent; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -352,7 +292,6 @@ COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
 
 
 --
--- TOC entry 9 (class 3079 OID 16776)
 -- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -360,8 +299,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 
 --
--- TOC entry 4127 (class 0 OID 0)
--- Dependencies: 9
 -- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -369,7 +306,6 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UU
 
 
 --
--- TOC entry 3 (class 3079 OID 16430)
 -- Name: xml2; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -377,8 +313,6 @@ CREATE EXTENSION IF NOT EXISTS xml2 WITH SCHEMA public;
 
 
 --
--- TOC entry 4128 (class 0 OID 0)
--- Dependencies: 3
 -- Name: EXTENSION xml2; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -390,7 +324,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 229 (class 1259 OID 39160)
 -- Name: urls; Type: TABLE; Schema: public; Owner: vghvwvps
 --
 
@@ -407,7 +340,6 @@ CREATE TABLE public.urls (
 ALTER TABLE public.urls OWNER TO vghvwvps;
 
 --
--- TOC entry 228 (class 1259 OID 39158)
 -- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: vghvwvps
 --
 
@@ -423,8 +355,6 @@ CREATE SEQUENCE public.urls_id_seq
 ALTER TABLE public.urls_id_seq OWNER TO vghvwvps;
 
 --
--- TOC entry 4129 (class 0 OID 0)
--- Dependencies: 228
 -- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vghvwvps
 --
 
@@ -432,7 +362,6 @@ ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
 
 
 --
--- TOC entry 227 (class 1259 OID 39150)
 -- Name: users; Type: TABLE; Schema: public; Owner: vghvwvps
 --
 
@@ -449,7 +378,6 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO vghvwvps;
 
 --
--- TOC entry 226 (class 1259 OID 39148)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: vghvwvps
 --
 
@@ -465,8 +393,6 @@ CREATE SEQUENCE public.users_id_seq
 ALTER TABLE public.users_id_seq OWNER TO vghvwvps;
 
 --
--- TOC entry 4130 (class 0 OID 0)
--- Dependencies: 226
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vghvwvps
 --
 
@@ -474,7 +400,6 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 3959 (class 2604 OID 39163)
 -- Name: urls id; Type: DEFAULT; Schema: public; Owner: vghvwvps
 --
 
@@ -482,7 +407,6 @@ ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id
 
 
 --
--- TOC entry 3956 (class 2604 OID 39153)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: vghvwvps
 --
 
@@ -490,8 +414,6 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 4101 (class 0 OID 39160)
--- Dependencies: 229
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: vghvwvps
 --
 
@@ -501,8 +423,6 @@ COPY public.urls (id, "idUser", "shortUrl", url, "visitCount", "createdAt") FROM
 
 
 --
--- TOC entry 4099 (class 0 OID 39150)
--- Dependencies: 227
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: vghvwvps
 --
 
@@ -512,8 +432,6 @@ COPY public.users (id, name, email, password, "linksCount", "createdAt") FROM st
 
 
 --
--- TOC entry 4131 (class 0 OID 0)
--- Dependencies: 228
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vghvwvps
 --
 
@@ -521,8 +439,6 @@ SELECT pg_catalog.setval('public.urls_id_seq', 1, true);
 
 
 --
--- TOC entry 4132 (class 0 OID 0)
--- Dependencies: 226
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vghvwvps
 --
 
@@ -530,7 +446,6 @@ SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
 --
--- TOC entry 3965 (class 2606 OID 39170)
 -- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: vghvwvps
 --
 
@@ -539,7 +454,6 @@ ALTER TABLE ONLY public.urls
 
 
 --
--- TOC entry 3963 (class 2606 OID 39157)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: vghvwvps
 --
 
@@ -548,7 +462,6 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 3966 (class 2606 OID 39171)
 -- Name: urls urls_idUser_fkey; Type: FK CONSTRAINT; Schema: public; Owner: vghvwvps
 --
 
@@ -557,16 +470,12 @@ ALTER TABLE ONLY public.urls
 
 
 --
--- TOC entry 4107 (class 0 OID 0)
--- Dependencies: 25
 -- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
 
 REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
-
--- Completed on 2023-08-07 16:40:31 -03
 
 --
 -- PostgreSQL database dump complete
