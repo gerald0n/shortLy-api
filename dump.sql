193ea29bfb208312c96915713461e9b9d1c7d276
Geraldo Gomes <contato.gneto@gmail.com>
2023-08-07 06:45:16 -0300

    add dump.sql

---
+++ dump.sql
@@ -0,0 +1,149 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.3 (Ubuntu 15.3-0ubuntu0.23.04.1)
+-- Dumped by pg_dump version 15.3 (Ubuntu 15.3-0ubuntu0.23.04.1)
+
+-- Started on 2023-08-07 06:44:26 -03
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 217 (class 1259 OID 16945)
+-- Name: urls; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.urls (
+    id integer NOT NULL,
+    "idUser" integer NOT NULL,
+    "shortUrl" character varying(10) NOT NULL,
+    url text NOT NULL,
+    "visitCount" integer DEFAULT 0,
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+--
+-- TOC entry 216 (class 1259 OID 16944)
+-- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.urls_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3364 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
+
+
+--
+-- TOC entry 215 (class 1259 OID 16936)
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    password character varying(100) NOT NULL,
+    "linksCount" integer DEFAULT 0,
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+--
+-- TOC entry 214 (class 1259 OID 16935)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3365 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- TOC entry 3209 (class 2604 OID 16948)
+-- Name: urls id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id_seq'::regclass);
+
+
+--
+-- TOC entry 3206 (class 2604 OID 16939)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- TOC entry 3215 (class 2606 OID 16954)
+-- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.urls
+    ADD CONSTRAINT urls_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3213 (class 2606 OID 16943)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3216 (class 2606 OID 16955)
+-- Name: urls urls_idUser_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.urls
+    ADD CONSTRAINT "urls_idUser_fkey" FOREIGN KEY ("idUser") REFERENCES public.users(id);
+
+
+-- Completed on 2023-08-07 06:44:27 -03
+
+--
+-- PostgreSQL database dump complete
+--
+
